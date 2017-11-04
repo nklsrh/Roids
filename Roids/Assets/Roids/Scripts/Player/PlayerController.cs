ea959163f8f4b187c6ebf128ffa7655f95c1ec1f
Nikhil Suresh <nklsrh@gmail.com>
2017-11-05 10:47:44 +1100

    + Cleaned up the player input and made it play more like Asteroids

--- Roids/Assets/Roids/Scripts/Player/PlayerController.cs
+++ Roids/Assets/Roids/Scripts/Player/PlayerController.cs
@@ -7,20 +7,27 @@ public class PlayerController : BaseObject
 
     //int score;
 
+    Vector3 velocityDirection;
+    Vector3 accelerationDirection;
+
     Vector3 velocity;
     Vector3 acceleration;
 
     public float thrustMultiplier = 0.1f;
+    public float rotationMultiplier = 1f;
 
     public float accelerationSlowdown = 10.0f;
     public float velocitySlowdown = 1.0f;
-    public bool slowdownAcclerationOnlyWhenNotThrusting = false;
-    public bool slowdownVelocityOnlyWhenNotThrusting = false;
+
+    public float accelerationSlowdownWhenNotThrusting = 10.0f;
+    public float velocitySlowdownWhenNotThrusting = 1.0f;
 
     float rotationAmount = 0;
     public float rotationSlowdown = 10.0f;
     public float maxRotationAmount = 10;
 
+    public float maximumVelocityMagnitudeSquared = 10.0f;
+    public float maximumAccelerationMagnitudeSquared = 1.0f;
 
     float currentThrustAmount = 0;
 
@@ -31,39 +38,55 @@ public class PlayerController : BaseObject
 
     public override void Logic ()
     {
+
+        Debug.DrawLine(transform.position, transform.position + velocity * 10, Color.yellow);
+        //Debug.DrawLine(transform.position, transform.position + acceleration * 100, Color.blue);
+
+        // MOVEMENT PHYSICS LOGIC
+
+
         velocity += acceleration;
-        transform.Translate(velocity * Time.deltaTime);
+
+        transform.Translate(velocity * Time.deltaTime, Space.World);
 
 
-        transform.Rotate(Vector3.up * rotationAmount);
+        // ROTATION LOGIC
 
-        rotationAmount = Mathf.Lerp(0, rotationAmount, rotationSlowdown * Time.deltaTime);
+        transform.Rotate(Vector3.up * rotationAmount * Time.deltaTime);
+
+        rotationAmount = Mathf.Lerp(rotationAmount, 0, rotationSlowdown * Time.deltaTime);
+
+
+
+        // WEAPON LOGIC
 
         weaponController.Logic();
 
 
-        if (!slowdownAcclerationOnlyWhenNotThrusting || (slowdownAcclerationOnlyWhenNotThrusting && currentThrustAmount == 0))
-        {
-            acceleration = Vector3.Lerp(acceleration, Vector3.zero, accelerationSlowdown * Time.deltaTime);
-        }
 
-        if (!slowdownVelocityOnlyWhenNotThrusting || (slowdownVelocityOnlyWhenNotThrusting && currentThrustAmount == 0))
-        {
-            velocity = Vector3.Lerp(velocity, Vector3.zero, velocitySlowdown * Time.deltaTime);
-        }
+        // MOVEMENT SLOWDOWN
 
-        //velocity = Vector3.Lerp(velocity, transform.forward * 0.1f, velocitySlowdown * Time.deltaTime);
+        bool isThrusting = currentThrustAmount > 0;
+        acceleration = Vector3.Lerp(acceleration, Vector3.zero, (isThrusting ? accelerationSlowdown : accelerationSlowdownWhenNotThrusting)  * Time.deltaTime);
+        velocity = Vector3.Lerp(velocity, Vector3.zero, (isThrusting ? velocitySlowdown : velocitySlowdownWhenNotThrusting) * Time.deltaTime);
+
+        currentThrustAmount = 0;
     }
 
     public void Turn(float amount)
     {
-        rotationAmount += amount;
+        rotationAmount += rotationMultiplier * amount;
         rotationAmount = Mathf.Clamp(rotationAmount, -maxRotationAmount, maxRotationAmount);
     }
 
     public void Thrust(float amount)
     {
-        acceleration += transform.forward * amount * thrustMultiplier;
+        Vector3 thrustAcceleration = transform.forward * amount * thrustMultiplier;
+
+        Debug.DrawLine(transform.position, transform.position + thrustAcceleration * 100, Color.red);
+        
+        acceleration += thrustAcceleration;
+
         currentThrustAmount = amount;
     }
 
