0fb7af8e121e0c7738d95bec098422138f8c61b3
Nikhil Suresh <nklsrh@gmail.com>
2017-11-08 21:41:02 +1100

    + Reorganise LevelController code files

--- Roids/Assets/Roids/Scripts/GameControl/LevelController.cs
+++ Roids/Assets/Roids/Scripts/GameControl/LevelController.cs
@@ -1,43 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System;
-
-[System.Serializable]
-public class Wave
-{
-    public enum ObjectiveType
-    {
-        None,
-        KillAll,
-        Protect,
-        Rebuild,
-        Survive
-    }
-
-    public enum EnemyType
-    {
-        Asteroid,
-        Saucer,
-        Juggernaut,
-        Stomper
-    }
-
-    public ObjectiveType objective;
-    public EnemyType enemyType;
-    public int enemyCount = 1;
-    public float difficulty = 0.0f;
-
-    public float duration = -1;
-
-    public bool IsTimeBased
-    {
-        get
-        {
-            return duration > 0;
-        }
-    }
-}
 
 public class LevelController : BaseObject
 {
---
+++ Roids/Assets/Roids/Scripts/GameControl/Wave.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Wave
+{
+    public enum ObjectiveType
+    {
+        None,
+        KillAll,
+        Protect,
+        Rebuild,
+        Survive
+    }
+
+    public enum EnemyType
+    {
+        Asteroid,
+        Saucer,
+        Juggernaut,
+        Stomper
+    }
+
+    public ObjectiveType objective;
+    public EnemyType enemyType;
+    public int enemyCount = 1;
+    public float difficulty = 0.0f;
+
+    public float duration = -1;
+
+    public bool IsTimeBased
+    {
+        get
+        {
+            return duration > 0;
+        }
+    }
+}
