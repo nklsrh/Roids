10fc90b829b8a908bf12eec9a70f778e2927126b
Nikhil Suresh <nklsrh@gmail.com>
2017-11-09 22:33:12 +1100

    + UI HUD System in progress, getting wave start/end and mission timer set up + Set up MainGameplayController which is the main point of entry for the game engine stuff + Secondary scene_game_uiTest used for the new UI stuff since there will be conflicts with the base scene from my other PC

---
+++ Roids/Assets/Roids/Scripts/HUD/UIHUDController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIHUDController : BaseObject 
+{
+	public UIHUDHealth health;
+	public UIHUDMission mission;
+	public UIHUDTimer timer;
+
+	public UIHUDNotifyWaveComplete notifyWaveComplete;
+
+	public override void Setup(){}
+
+	public void Setup (GameDirector gameDirector) 
+	{
+		gameDirector.onWaveStarted += OnWaveStarted;
+		gameDirector.onWaveComplete += OnWaveComplete;
+	}
+
+	public override void Logic()
+	{
+
+	}
+	
+	void OnWaveStarted(Wave wave)
+	{
+		timer.Disable();
+		if (wave.IsTimeBased)
+		{
+			timer.Enable();
+			timer.StartTimer(wave.duration);
+		}
+
+		mission.Enable();
+		mission.SetText(wave.objective.ToString());
+
+		notifyWaveComplete.Popup("NEW MISSION: " + wave.objective.ToString(), 1.5f);
+	}
+
+	void OnWaveComplete(Wave wave)
+	{
+		Debug.Log("ON WAVE COMPLETE");
+
+		timer.Disable();
+		notifyWaveComplete.Popup("Wave Complete", 1.5f);
+	}
+}
---
+++ Roids/Assets/Roids/Scripts/HUD/UIHUDMission.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIHUDMission : MonoBehaviour 
+{
+	[SerializeField]
+	TMPro.TextMeshProUGUI txtMission;
+
+	public void Enable()
+	{
+		gameObject.SetActive(true);
+	}
+
+	public void Disable()
+	{
+		gameObject.SetActive(false);
+	}
+
+	public void SetText(string text)
+	{
+		txtMission.text = text;
+	}
+}
